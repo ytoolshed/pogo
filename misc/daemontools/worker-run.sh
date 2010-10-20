84491aaa5c8bbc5b2c042c8d769fbeffd33d6250
Nicholas Harteau <nrh@ikami.com>
2010-10-19 20:25:06 -0400

    Add non-working worker code

---
+++ misc/daemontools/worker-run.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Copyright (c) 2010, Yahoo! Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+WORKER="/usr/local/sbin/pogo-worker"
+WORKER_KEY="/usr/local/etc/pogo/worker.key"
+RUN_AS_USER="root"
+
+exec 2>&1
+
+if [ ! -f "$WORKER_KEY" ]; then
+  echo "ERROR cannot start pogo-worker: $WORKER_KEY is missing"
+  exit 1
+fi
+
+echo "starting pogo-worker"
+
+# linux only?
+if [ -w "/proc/sys/fs/file-max" ]; then
+  echo "131072" > /proc/sys/fs/file-max
+fi
+
+# hopefully somewhat portable
+ulimit -n 131072
+exec setuidgid $RUN_AS_USER $WORKER
+
---
+++ t/lib/DummyWorker.pm
@@ -0,0 +1,86 @@
+package PogoDummyWorker;
+
+# Copyright (c) 2010, Yahoo! Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+use Pogo::Worker;
+
+our $opts = {};
+our $execute = undef;
+sub instance()
+{
+  if ( defined $opts->{log4perl} && -r $opts->{log4perl} )
+  {
+    Log::Log4perl::init( $opts->{log4perl} );
+  }
+  if ( defined $opts->{loglevel} )
+  {
+    Log::Log4perl::get_logger()->level( $opts->{loglevel} );
+  }
+
+  {
+    die "Please set PogoDummyWorker::execute to a coderef" unless ref $execute eq 'CODE';
+
+    no strict 'refs';
+    *{'Pogo::Worker::Connection::real_execute'} = \&Pogo::Worker::Connection::execute;
+    *{'Pogo::Worker::Connection::execute'} = $execute;
+  }
+  return Pogo::Worker->instance($opts);
+}
+
+"The proles don't matter.";
+
+1;
+
+=pod
+
+=head1 NAME
+
+  CLASSNAME - SHORT DESCRIPTION
+
+=head1 SYNOPSIS
+
+CODE GOES HERE
+
+=head1 DESCRIPTION
+
+LONG_DESCRIPTION
+
+=head1 METHODS
+
+B<methodexample>
+
+=over 2
+
+methoddescription
+
+=back
+
+=head1 SEE ALSO
+
+L<Pogo::Dispatcher>
+
+=head1 COPYRIGHT
+
+Apache 2.0
+
+=head1 AUTHORS
+
+  Andrew Sloane <asloane@yahoo-inc.com>
+  Michael Fischer <mfischer@yahoo-inc.com>
+  Nicholas Harteau <nrh@yahoo-inc.com>
+  Nick Purvis <nep@yahoo-inc.com>
+  Robert Phan <rphan@yahoo-inc.com>
+
+=cut
