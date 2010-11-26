cfacbf850cfe0d7736398f2d27ad84095375f260
nicholas harteau <nrh@ikami.com>
2010-11-26 15:08:00 -0500

    inline plugin and plugin base class

---
+++ lib/Pogo/Plugin/Target.pm
@@ -0,0 +1,70 @@
+package Pogo::Plugin::Target;
+
+# Copyright (c) 2010, Yahoo! Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+1;
+
+sub new
+{
+  my $class = shift;
+  my $self = {};
+
+  bless $self, $class;
+  return $self;
+}
+
+=pod
+
+=head1 NAME
+
+  CLASSNAME - SHORT DESCRIPTION
+
+=head1 SYNOPSIS
+
+CODE GOES HERE
+
+=head1 DESCRIPTION
+
+LONG_DESCRIPTION
+
+=head1 METHODS
+
+B<methodexample>
+
+=over 2
+
+methoddescription
+
+=back
+
+=head1 SEE ALSO
+
+L<Pogo::Dispatcher>
+
+=head1 COPYRIGHT
+
+Apache 2.0
+
+=head1 AUTHORS
+
+  Andrew Sloane <asloane@yahoo-inc.com>
+  Michael Fischer <mfischer@yahoo-inc.com>
+  Nicholas Harteau <nrh@yahoo-inc.com>
+  Nick Purvis <nep@yahoo-inc.com>
+  Robert Phan <rphan@yahoo-inc.com>
+
+=cut
+
+# vim:syn=perl:sw=2:ts=2:sts=2:et:fdm=marker
