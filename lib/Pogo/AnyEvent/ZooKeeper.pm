673962a980ee939aabba520fc8675f58f76e6582
mschilli <github@perlmeister.com>
2012-02-10 21:00:30 -0800

    zk component code complete

--- lib/Pogo/AnyEvent/ZooKeeper.pm
+++ lib/Pogo/AnyEvent/ZooKeeper.pm
@@ -21,21 +21,34 @@ sub new {
         zk_host                   => "localhost",
         zk_port                   => 2181,
         zk_connect_retry_interval => 5,
+        zk_method_retries         => 2,
+        zk_method_interval        => 10,
         connected                 => 0,
         args                      => \@opts,
-        settings                  => {},
+        zk_options                => { 
+            data_read_len => 1023
+        },
     };
 
     bless $self, $class;
 
-    $self->{ qp } = Pogo::Util::QP->new( timeout => 5 );
-    $self->{ qp }->reg_cb( "next", $self->connect_handler() );
-
+      # connect/reconnect
+    $self->{ connector } = Pogo::Util::QP->new( 
+        timeout => $self->{ zk_connect_retry_interval } );
+    $self->{ connector }->reg_cb( "next", $self->connect_handler() );
     $self->reg_cb( "connect", sub {
         DEBUG "Received request to connect to ZK";
         $self->{ connected } = 0;
-        $self->{ qp }->push( $self->{ zk_host }, $self->{ zk_port } );
+        $self->{ connector }->push( $self->{ zk_host }, $self->{ zk_port } );
     } );
+
+      # command queuing
+    $self->{ cmd_queue } = Pogo::Util::QP->new(
+        timeout => $self->{ zk_method_interval },
+        retries => $self->{ zk_method_retries } );
+    $self->{ cmd_queue}->reg_cb( "next", $self->cmd_handler() );
+
+    $self->mk_methods();
 
     return $self;
 }
@@ -46,6 +59,8 @@ sub ping {
     my( $self ) = @_;
 
     DEBUG "Testing ZK connection";
+
+    return 0 if ! defined $self->{ zk };
 
     my $rc = $self->{ zk }->get( "/" );
 
@@ -77,14 +92,17 @@ sub connect_handler {
         my $port = $self->{ zk_port };
 
         DEBUG "Connecting to ZK on $host:$port";
-        $self->{ zk } = Net::ZooKeeper->new( "$host:$port" );
+        $self->{ zk } = Net::ZooKeeper->new( 
+            "$host:$port",
+            %{ $self->{ zk_options } },
+        );
 
         my $rc = $self->ping();
 
         if( defined $rc ) {
             INFO "Connected to ZK on $host:$port";
             $self->{ connected } = 1;
-            $self->{ qp }->ack();
+            $self->{ connector }->ack();
             $self->event( "zk_connect_ok" );
         } else {
             $self->event( "zk_connect_error", $self->{ zk }->get_error() );
@@ -102,149 +120,119 @@ sub netloc {
     return "$self->{ zk_host }:$self->{ zk_port }";
 }
 
-# TODO
-
 ###########################################
-sub reconnect {
+sub get_error {
 ###########################################
-    my( $self ) = @_;
+    my( $self, @args ) = @_;
 
-    # According to the Net::ZooKeeper docs: 
-    # On occasion the ZooKeeper client code may not be able to quickly
-    # reconnect to a live server and the caller may want to destroy
-    # the existing Net::ZooKeeper handle object and attempt a
-    # fresh connection using the same session ID as before with a
-    # new Net::ZooKeeper object.  To do so, save the C<session_id>
-    # attribute value before undefining the old handle object
-    #     and then pass that binary string as the value of the
-    # C<'session_id'> option to the C<new()> method when creating the
-    # next handle object.
-
-    INFO "Trying to reconnect with ZooKeeper: @{ $self->{ args } }";
-
-    $self->{ zk } = Net::ZooKeeper->new( @{ $self->{ args } } );
-    $self->_settings_apply();
-    DEBUG "Got new ZooKeeper handle: ", Dumper( $self->{zk} );
-
-    $self->event( "connected", $self->{zk} );
-}
-
-###########################################
-sub scalar2str {
-###########################################
-    if(! defined $_[0]) {
-        return '[undef]';
+    if( !defined $self->{zk} ) {
+        return "No connected to ZK yet.";
     }
-    return $_[0];
+
+    return $self->{zk}->get_error( @args );
 }
 
 ###########################################
-sub array2str {
+sub mk_method {
 ###########################################
-    my @array = @_; # copy to modify it in-place
-    join ', ', map { scalar2str $_ } @array;
-}
+    my( $self, $method ) = @_;
 
-  # This logs the ZK method calls and all the arguments we pass
-  # to get a better idea what goes in and out of ZooKeeper
-for my $method (qw(
-    create set exists get delete get_children
-    add_auth get_acl set_acl stat watch
-)) {
+    # Make a wrapper of a Net::ZooKeeper function, supporting logging,
+    # reconnects, and retries.
+
     no strict 'refs';
 
-    *{"Pogo::AnyEvent::ZooKeeper::$method"} = sub {
+
+    my $pkg = __PACKAGE__;
+
+    *{"$pkg::$method"} = sub {
         my($self, @args) = @_;
 
         my $wantarray = wantarray;
-        my @rc;
 
-        # We perform exactly one immediate retry if we get a 
-        # connection error, after trying to reconnect.
-        for(1..2) {
-            if( $wantarray ) {
-                @rc = $self->{zk}->$method( @args );
-            } else {
-                $rc[0] = $self->{zk}->$method( @args );
-            }
+          # last arg is callback by convention
+        my $callback = pop @args;
 
-            if ( $self->is_hosed() ) {
-                ERROR "ZK connection error: ", $self->{zk}->get_error();
-
-                # [bug 4709031] From: ZooKeeper.pm
-                # "If the error code is greater than ZAPIERROR, then a 
-                # connection error or server error has occurred and the 
-                # client should probably close the connection by undefining 
-                # the Net::ZooKeeper handle object and, if necessary, attempt 
-                # to create a new connection to the ZooKeeper cluster."
-
-                $self->{old_session_id} = $self->{zk}->{session_id};
-                $self->{zk} = undef;
-                $self->reconnect();
-                next;
-            }
-
-            last;
-        }
-
-          # we want Log4perl to log the code location where this
-          # call came from, not this layer
-        local $Log::Log4perl::caller_depth = 
-              $Log::Log4perl::caller_depth  + 1;
-
-        my $sub = sub {
-            my $last_ret = $self->{zk}->get_error();
-            my $session_id = $self->{zk}->{session_id};
-           
-            if( defined $session_id and length $session_id ) {
-                $session_id = sprintf "0x%x", 
-                    unpack( "q", $session_id );
-            } else {
-                $session_id = "[no session]";
-            }
-
-            my $info = "ZK $session_id $method(" . array2str( @args ) . ")=>";
-
-            if( $wantarray ) {
-                my $result = array2str( @rc );
-
-                $info .= logresult( $method, "@args", $result );
-
-                if( @rc == 0 ){
-                    $info .= " (err=" . $self->get_error() . ")";
-                } else {
-                    $info .= " (OK)";
-                }
-
-            } else {
-                if( defined $rc[0] ){
-                    $info .= "(rc=" . scalar2str( $rc[0] ) . ")";
-                } else {
-                    $info .= "(err=" . $self->get_error() . ")";
-                }
-            }
-
-            return $info;
-        };
-
-        my $zk_error = $self->{zk}->get_error();
-        if( $zk_error and $zk_error > ZAPIERROR) {
-            INFO $sub;
-        } else {
-            DEBUG $sub;
-        }
-
-        if( $wantarray ) {
-            return @rc;
-        }
-        return $rc[0];
+        $self->{ cmd_queue }->push(
+          { method    => $method,
+            callback  => $callback,
+            args      => \@args,
+            wantarray => $wantarray,
+          }
+        );
     };
+}
+
+###########################################
+sub cmd_handler {
+###########################################
+    my( $self, $data ) = @_;
+
+    my $method    = $data->{ method };
+    my $callback  = $data->{ callback };
+    my @args      = @{ $data->{ args } };
+    my $wantarray = $data->{ wantarray };
+    my $logger    = get_logger();
+
+    my @rc;
+
+    no strict 'refs';
+
+    if( $wantarray ) {
+        @rc = $self->{zk}->$method( @args );
+    } else {
+        $rc[0] = $self->{zk}->$method( @args );
+    }
+
+    if ( $self->is_hosed() ) {
+        ERROR "ZK connection error: ", $self->get_error();
+
+        # ZooKeeper.pm says:
+        # "If the error code is greater than ZAPIERROR, then a 
+        # connection error or server error has occurred and the 
+        # client should probably close the connection by undefining 
+        # the Net::ZooKeeper handle object and, if necessary, attempt 
+        # to create a new connection to the ZooKeeper cluster."
+
+        # try to re-establish a lost ZK connection
+        $self->event( "connect" );
+
+        # Not sending ACK, current operation will be retried
+    } else {
+        # Success.
+        $self->{ cmd_queue }->ack();
+        if( $logger->is_debug() ) {
+            $self->cmd_log( $method, \@args, \@rc );
+        }
+        $callback->( $self, @rc );
+    }
+}
+
+###########################################
+sub cmd_log {
+###########################################
+    my( $self, $method, $args, $rc ) = @_;
+
+    DEBUG "ZK: $method args=", Dumper( $args ), " rc=", Dumper( $rc );
+}
+
+###########################################
+sub mk_methods {
+###########################################
+    my( $self ) = @_;
+
+    for my $method (qw(
+        create set exists get delete get_children
+        add_auth get_acl set_acl stat watch
+    )) {
+        $self->mk_method( $method );
+    }
 }
 
 ###########################################
 sub is_hosed {
 ###########################################
-    my( $self, @args ) = @_;
+    my( $self ) = @_;
 
     my $err =  $self->{zk}->get_error();
 
@@ -257,37 +245,6 @@ sub is_hosed {
       }
 
       return 0;
-}
-
-###########################################
-sub get_error {
-###########################################
-    my( $self, @args ) = @_;
-
-    return $self->{zk}->get_error( @args );
-}
-
-###########################################
-sub data_read_len {
-###########################################
-    my( $self, $value ) = @_;
-
-    if( defined $value ) {
-        $self->{ settings }->{ data_read_len } = $value;
-        $self->_settings_apply();
-    }
-
-    return $self->{ settings }->{ data_read_len };
-}
-
-###########################################
-sub _settings_apply {
-###########################################
-    my( $self ) = @_;
-
-    for my $key ( keys %{ $self->{ settings } } ) {
-        $self->{zk}->{ $key } = $self->{ settings }->{ $key };
-    }
 }
 
 1;
@@ -307,7 +264,7 @@ Pogo::AnyEvent::ZooKeeper - Event-based ZooKeeper Interface
     my $zk = Pogo::AnyEvent::ZooKeeper->new();
 
     $zk->create( "/some/node", sub {
-        my( $key, $value ) = @_;
+        my( $rc, $key, $value ) = @_;
 
         $cv->send();
     });
