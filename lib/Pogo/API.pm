4773eb87cfa87be77f3b34c44e4f5f7e4a296eb3
mschilli <github@perlmeister.com>
2012-02-17 11:15:51 -0800

    added main API

---
+++ lib/Pogo/API.pm
@@ -0,0 +1,96 @@
+###########################################
+package Pogo::API;
+###########################################
+use strict;
+use warnings;
+use Plack::App::URLMap;
+use Log::Log4perl qw(:easy);
+
+###########################################
+sub app {
+###########################################
+    my( $class ) = @_;
+
+    my $app = Plack::App::URLMap->new();
+
+      # map URLs to modules, like /status => API/Status.pm etc.
+    for my $api ( qw( status v1 ) ) {
+
+        my $module = __PACKAGE__;
+        $module =~ s/::[^:]*$//;
+        $module .= "::" . ucfirst( $api );
+
+        eval "require $module";
+        if( $@ ) {
+            die "Failed to load module $module ($@)";
+        }
+
+        DEBUG "Mounting /$api to module $module";
+
+        $app->mount( "/$api" => $module->app( ) );
+    }
+
+    return $app;
+}
+
+my $app = __PACKAGE__->app();
+
+__END__
+
+=head1 NAME
+
+Pogo::Dispatcher::API - Pogo API PSGI interface
+
+=head1 SYNOPSIS
+
+    use Pogo::API;
+    my $app = Pogo::API->app();
+
+=head1 DESCRIPTION
+
+App handler for Pogo's main Web API. Can be used either by a standalone 
+server (by calling
+
+    $ plackup lib/Pogo/API.pm
+
+directly or in the test suite) or in apache with C<Plack::Handler::Apache1>.
+The apache configuration looks like this:
+
+    <Location />
+     SetHandler perl-script
+     PerlHandler Plack::Handler::Apache1
+     PerlSetVar psgi_app /path/to/app.psgi
+    </Location>
+
+=head1 LICENSE
+
+Copyright (c) 2010-2012 Yahoo! Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+imitations under the License.
+
+=head1 AUTHORS
+
+Mike Schilli <m@perlmeister.com>
+Ian Bettinger <ibettinger@yahoo.com>
+
+Many thanks to the following folks for implementing the
+original version of Pogo: 
+
+Andrew Sloane <andy@a1k0n.net>, 
+Michael Fischer <michael+pogo@dynamine.net>,
+Nicholas Harteau <nrh@hep.cat>,
+Nick Purvis <nep@noisetu.be>,
+Robert Phan <robert.phan@gmail.com>,
+Srini Singanallur <ssingan@yahoo.com>,
+Yogesh Natarajan <yogesh_ny@yahoo.co.in>
+
---
+++ lib/Pogo/API/V1.pm
@@ -0,0 +1,110 @@
+###########################################
+package Pogo::API::V1;
+###########################################
+use strict;
+use warnings;
+use Log::Log4perl qw(:easy);
+use JSON qw( to_json );
+use Pogo::Util qw( http_response_json );
+use HTTP::Status qw( :constants );
+use Plack::Request;
+use Data::Dumper;
+
+###########################################
+sub app {
+###########################################
+    my( $class, $dispatcher ) = @_;
+
+    return sub {
+        my( $env ) = @_;
+
+        DEBUG "Got v1 request";
+
+        my $path = $env->{ PATH_INFO };
+        ( my $command = $path ) =~ s#^/##;
+
+        my %commands = map { $_ => 1} qw( jobinfo jobsubmit );
+
+        if( exists $commands{ $command } ) {
+            no strict 'refs';
+            return $command->( $env );
+        }
+
+        return http_response_json(
+            { error => [ "unknown request: '$path'" ] }, 
+            HTTP_BAD_REQUEST,
+        );
+    };
+}
+
+###########################################
+sub jobinfo {
+###########################################
+    my( $env ) = @_;
+
+    my $req = Plack::Request->new( $env );
+
+    my $params = $req->parameters();
+
+    if( exists $params->{ jobid } ) {
+
+        return http_response_json(
+            { rc      => "ok",
+              message => "jobid $params->{ jobid }", 
+            }
+        );
+    }
+
+    return http_response_json(
+        { rc      => "error",
+          message => "jobid missing", 
+        }
+    );
+}
+
+1;
+
+__END__
+
+=head1 NAME
+
+Pogo::API::V1 - Pogo API Handlers
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+Handles URLs like C</v1/jobstatus>, C</v1/jobsubmit>, etc.
+
+=head1 LICENSE
+
+Copyright (c) 2010-2012 Yahoo! Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+imitations under the License.
+
+=head1 AUTHORS
+
+Mike Schilli <m@perlmeister.com>
+Ian Bettinger <ibettinger@yahoo.com>
+
+Many thanks to the following folks for implementing the
+original version of Pogo: 
+
+Andrew Sloane <andy@a1k0n.net>, 
+Michael Fischer <michael+pogo@dynamine.net>,
+Nicholas Harteau <nrh@hep.cat>,
+Nick Purvis <nep@noisetu.be>,
+Robert Phan <robert.phan@gmail.com>,
+Srini Singanallur <ssingan@yahoo.com>,
+Yogesh Natarajan <yogesh_ny@yahoo.co.in>
+
